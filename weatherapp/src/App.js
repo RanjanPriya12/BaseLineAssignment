dee12dbc4d1335af623648a984170d7a6a158813
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2022-09-24 21:13:29 +0530

    commit

---
+++ weatherapp/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Input } from './components/input';
+
+function App() {
+ 
+  return (
+    <div className="App">
+     
+<Input/>
+    </div>
+  );
+}
+
+export default App;
